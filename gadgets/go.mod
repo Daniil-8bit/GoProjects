118c179e8bfda02e9e8a61ad49424974c6416980
Daniil-8bit <danshin.daniil@gmail.com>
2025-01-23 09:21:36 +0300

    check gadget import

---
+++ gadgets/go.mod
@@ -0,0 +1,3 @@
+module gadgetsMod
+
+go 1.23.4
--- gadgets/tape.go
+++ gadgets/tape.go
@@ -29,3 +29,16 @@ func (tp TapePlayer) Play(song string) {
 func (tp TapePlayer) Stop() {
 	fmt.Println("Music stopped!")
 }
+
+func main() {
+	list := []string{"Song 1", "Song 2", "Song 3"}
+	device := TapePlayer{}
+	playSongs(device, list)
+}
+
+func playSongs(gadget TapePlayer, songs []string) {
+	for _, elem := range songs {
+		gadget.Play(elem)
+	}
+	gadget.Stop()
+}
